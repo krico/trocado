7c252ae6820fb54ca0a22119013f17c520f19245
krico <github@cwa.to>
2016-02-14 23:15:10 -0500

    gulp build now supports less stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,55 @@
 var gulp = require('gulp');
 var plug = require('gulp-load-plugins')();
 
+var paths = {
+    target: {
+        webapp: 'target/trocado-1.0-SNAPSHOT',
+        build: 'target/trocado-1.0-SNAPSHOT/build'
+    },
+    client: {
+        js: ['src/main/client/**/*.module.js', 'src/main/client/**/*.js'],
+        markup: ['src/main/webapp/**/*.html'],
+        style: ['src/main/styles/**/*.less']
+    }
+};
+
 gulp.task('client-js', clientJs);
 gulp.task('client-markup', clientMarkup);
-gulp.task('default', ['client-js', 'client-markup']);
+gulp.task('client-style', clientStyle);
+gulp.task('watch', watchIt);
+
+gulp.task('build', ['client-js', 'client-markup', 'client-style']);
+gulp.task('default', ['build', 'watch']);
+
+function watchIt() {
+    gulp.watch(paths.client.js, ['client-js']);
+    gulp.watch(paths.client.markup, ['client-markup']);
+    gulp.watch(paths.client.style, ['client-style']);
+}
+
+function clientStyle() {
+    return gulp.src(paths.client.style)
+        .pipe(plug.plumber())
+        .pipe(plug.less())
+        .pipe(plug.concat('trocado.css'))
+        .pipe(gulp.dest(paths.target.build));
+}
 
 function clientJs() {
-    var dest = 'target/trocado-1.0-SNAPSHOT/build';
-    return gulp.src(["src/main/client/**/*.module.js",
-        "src/main/client/**/*.js"])
+    return gulp.src(paths.client.js)
         .pipe(plug.plumber())
         .pipe(plug.ngAnnotate())
         .pipe(plug.sourcemaps.init())
         .pipe(plug.concat('trocado.js'))
-        .pipe(gulp.dest(dest))
+        .pipe(gulp.dest(paths.target.build))
         .pipe(plug.uglify())
         .pipe(plug.rename({extname: '.min.js'}))
         .pipe(plug.sourcemaps.write('./'))
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest(paths.target.build));
 }
 
 function clientMarkup() {
-    var dest = 'target/trocado-1.0-SNAPSHOT';
-    return gulp.src(["src/main/webapp/**/*.html"])
+    return gulp.src(paths.client.markup)
         .pipe(plug.plumber())
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest(paths.target.webapp));
 }
